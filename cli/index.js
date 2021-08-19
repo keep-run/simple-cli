9ee8353b3a7ae9b8723a41c5abfe0c311c6c1ae6
Eleven <zhichun.wzc@autonavi.com>
2021-08-19 15:33:38 +0800

    删除无用代码

--- cli/index.js
+++ cli/index.js
@@ -1,18 +1,3 @@
 #!/usr/bin/env node
 // 很重要：指定node执行环境
-// var fs=require('fs')
-// var path =require('path')
-// require('@babel/polyfill')
-// require('@babel/core')
-// require('@babel/register')({
-//     presets: [require.resolve('@babel/preset-env')],
-//     ignore: [],
-//     // cache: true,
-//   })
-// // require('@babel/register')
-// // console.log(111111,require('./babel.config'))
-// // require('@babel/register')(require('./babel.config'))
-// console.log(12345687650987654321)
-// require('./src/index')
-// console.log('---index.js-----')
 require('./lib/index')
--- cli/src/index.js
+++ cli/src/index.js
@@ -1,18 +1,3 @@
-// import optimist from 'optimist'
-// import fs from 'fs'
-// import path from 'path'
-
-// const { argv } = optimist
-// const commands = argv._
-// const clis = fs.readdirSync(path.resolve(__dirname, 'clis')).map(item => item.replace('.js', ''))
-// const cmd = clis.indexOf(commands[0]) > -1 ? commands[0] : ''
-// if (cmd) {
-//     const command = require(`./clis/${cmd}`).default
-//     argv.cwd = process.cwd()
-//     command(argv)
-// } else {
-//     console.log('not found')
-// }
 
 import { program } from 'commander'
 const pkg = require('../package.json')
