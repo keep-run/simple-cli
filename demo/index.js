d4d995e1566a9709f8d70266f649cb291b943520
Eleven <zhichun.wzc@autonavi.com>
2021-09-15 21:06:01 +0800

    feat:完善demo

--- demo/index.js
+++ demo/index.js
@@ -1,1 +1,36 @@
-document.body.innerHTML = '<div>demo</div>'
+import React from 'react'
+import { render } from 'react-dom'
+import './index.css'
+import './index.less'
+import './index.styl'
+import imgSrc from './img/test.jpg'
+
+function handleFetch() {
+  fetch('/test', {
+    method: 'get',
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json',
+      "X-Requested-With": "XMLHttpRequest"    //手动增加字段以识别ajax请求
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
+    });
+}
+render(
+  <div className="container">
+    <div>this is a react app 1</div>
+    <button onClick={() => { handleFetch() }}>test</button>
+    <img
+      width='537'
+      height='342'
+      src={imgSrc}
+    // src='https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_960_720.jpg'
+    />
+  </div>,
+  document.body
+)
